bc2a067d218f88f1c14d0b1060969ed4f89e507e
Anton Hunt <>
2019-08-08 20:56:28 -0400

    Adding the .gitignore file

---
+++ CarFaxInterview/CarFaxInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+// ViewController.swift
+// 
+//
+// Created by My Name and Ohter Name on 8/8/19.
+// Copyright © 2019 Your Company Name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
